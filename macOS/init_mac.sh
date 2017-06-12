9e2209421ff430274c09f9f240fffe3867389773
Sliverwing <tlv591208510@gmail.com>
2017-06-12 20:17:18 +0800

    add alias for php artisan

--- macOS/init_mac.sh
+++ macOS/init_mac.sh
@@ -29,5 +29,6 @@ git config --global user.name "Sliverwing"
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+# Add alias
+echo "alias pa=\"php artisan\"" >> ~/.zshrc
 
