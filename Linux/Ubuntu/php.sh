9029e0901dddc131770133667994b2a4cc9082f7
Sliverwing <tlv591208510@gmail.com>
2017-07-05 18:13:51 +0800

    use mirror for composer

--- Linux/Ubuntu/php.sh
+++ Linux/Ubuntu/php.sh
@@ -1,1 +1,3 @@
 sudo apt install php7.0 php7.0-mbstring php7.0-xml php7.0-mysql php7.0-zip composer -y
+# set up composer mirror
+composer config -g repo.packagist composer https://packagist.laravel-china.org
