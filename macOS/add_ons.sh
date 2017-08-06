31e6bcbe9e6c8474ba789b4bb0f704a62ee68db8
Sliverwing <tlv591208510@gmail.com>
2017-08-06 14:09:44 +0800

    updated

--- macOS/add_ons.sh
+++ macOS/add_ons.sh
@@ -2,3 +2,15 @@
 
 # osx-terminal-themes
 git clone https://github.com/lysyi3m/osx-terminal-themes ~/.osx-terminal-themes
+
+#
+brew install vim tmux polipo
+
+# brew cask
+brew cask install google-chrome
+brew cask install neteasemusic
+brew cask install visual-studio-code
+
+# proxy
+echo "socksParentProxy=localhost:1080" > /usr/local/etc/polipo.conf
+echo "alias polipo=\"polipo -c /var/local/etc/polipo.conf\"" >> ~/.zshrc
